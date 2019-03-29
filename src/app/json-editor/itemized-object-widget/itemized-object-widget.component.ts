66b4b5650466b445d15a2abe038ebc8d1efb5cd6
Paul Holden <pholden@stria.com>
2019-03-29 16:33:41 -0700

    Add Copy Task/Item button

--- src/app/json-editor/itemized-object-widget/itemized-object-widget.component.ts
+++ src/app/json-editor/itemized-object-widget/itemized-object-widget.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { ArrayWidget } from 'ngx-schema-form';
 
+declare var M: any;
+
 @Component({
   selector: 'app-itemized-object-widget',
   templateUrl: './itemized-object-widget.component.html',
   styleUrls: ['./itemized-object-widget.component.css']
 })
-export class ItemizedObjectWidgetComponent extends ArrayWidget { }
+export class ItemizedObjectWidgetComponent extends ArrayWidget {
+  copyItem(item) {
+    let newItem = this.formProperty.addItem();
+    newItem.setValue(JSON.parse(JSON.stringify(item.value)), false);
+    setTimeout(() => {
+      M.updateTextFields();
+    }, 100);
+  }
+}
--- src/app/springcm/springcm-import/springcm-import-tasks-widget/springcm-import-tasks-widget.component.ts
+++ src/app/springcm/springcm-import/springcm-import-tasks-widget/springcm-import-tasks-widget.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { ArrayWidget } from 'ngx-schema-form';
 
+declare var M: any;
+
 @Component({
   selector: 'app-springcm-import-tasks-widget',
   templateUrl: './springcm-import-tasks-widget.component.html',
   styleUrls: ['./springcm-import-tasks-widget.component.scss']
 })
-export class SpringcmImportTasksWidgetComponent extends ArrayWidget { }
+export class SpringcmImportTasksWidgetComponent extends ArrayWidget {
+  copyItem(item) {
+    let newItem = this.formProperty.addItem();
+    newItem.setValue(JSON.parse(JSON.stringify(item.value)), false);
+    setTimeout(() => {
+      M.updateTextFields();
+    }, 100);
+  }
+}
